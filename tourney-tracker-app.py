defce6d68b759357dd2ef6a9cf502cd2bd243ffa
James Miller <jammiller.mba@gmail.com>
2022-06-30 16:49:41 -0400

    Kicking off things

---
+++ tourney-tracker-app.py
@@ -0,0 +1,11 @@
+print("Welcome to Tournaments R Us")
+print("============================")
+num_partipipants = input("Enter the number of participants: ")
+
+print("\n")
+print("There are " + str(num_partipipants) + " participant spots ready for sign-ups")
+
+print("\n")
+print("Participant Menu")
+print("=================")
+print("1. Sign Up \n2. Cancel Sign Up \n3. View Participants \n4. Save changes \n5. Exit")
