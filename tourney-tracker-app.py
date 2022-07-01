b62424702eb3cf02cfe21f40a70298fe3fda42d5
James Miller <jammiller.mba@gmail.com>
2022-07-01 09:51:24 -0400

    starting to write the functions

--- tourney-tracker-app.py
+++ tourney-tracker-app.py
@@ -1,11 +1,60 @@
-print("Welcome to Tournaments R Us")
-print("============================")
-num_partipipants = input("Enter the number of participants: ")
+num_participants = 0
+# participant_name = ''
 
-print("\n")
-print("There are " + str(num_partipipants) + " participant spots ready for sign-ups")
+def MenuSelection(option):
+    print("You selected option " + str(option))
+    if option == 1:
+        print("Participant Sign Up\n====================")
+        participant_name
+        participant_name = input("Participant Name: ")
+        desired_slot = input("Desired starting slot #:")
+    elif option == 2:
+        WelcomeScreen()
+    # elif option == 3:
+    #     # do something
+    # elif option == 4:
+    #     # do something 
+    elif option == 5:
+        exit()
 
-print("\n")
-print("Participant Menu")
-print("=================")
-print("1. Sign Up \n2. Cancel Sign Up \n3. View Participants \n4. Save changes \n5. Exit")
+
+def WelcomeScreen():
+    print("Welcome to Tournaments R Us")
+    print("============================")
+    global num_participants
+    num_participants = input("Enter the number of participants: ")
+    ParticipantMenu(num_participants)
+
+
+def ParticipantMenu(headcount):    
+    print("There are " + str(headcount) + " participant spots ready for sign-ups\n")
+    print("Participant Menu")
+    print("=================")
+    print("1. Sign Up \n2. Cancel Sign Up \n3. View Participants \n4. Save changes \n5. Exit")
+    print("\n(Type the number and press enter to navigate)")
+
+    menu_selection = input("What would you like to do? ")
+    print("menu_selection was: " + str(menu_selection))
+    MenuSelection(menu_selection)
+
+WelcomeScreen()
+
+# print("\n")
+# print("There are " + str(num_participants) + " participant spots ready for sign-ups")
+
+# print("\n")
+# print("Participant Menu")
+# print("=================")
+# print("1. Sign Up \n2. Cancel Sign Up \n3. View Participants \n4. Save changes \n5. Exit")
+# print("\n(Type the number and press enter to navigate)")
+
+# menu_selection = input("What would you like to do? ")
+# print("menu_selection was: " + str(menu_selection))
+# MenuSelection(menu_selection)
+
+# if menu_selection == 1:
+#         print("Participant Sign Up\n====================")
+#         participant_name
+#         participant_name = input("Participant Name: ")
+#         desired_slot = input("Desired starting slot #:")
+
